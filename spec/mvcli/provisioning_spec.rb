ce37ebf4b36fbbb7a6b797ad0e1836ad62dd4a91
Charles Lowell <cowboyd@thefrontside.net>
2013-06-04 14:44:37 -0500

    rename container -> scope, which is what it is

--- spec/mvcli/provisioning_spec.rb
+++ spec/mvcli/provisioning_spec.rb
@@ -8,19 +8,19 @@ describe "Provisioning" do
     Given(:mod) {Module.new {include MVCLI::Provisioning}}
     Given(:cls) {m = mod; Class.new {include m}}
     Given(:obj) {cls.new}
-    Given(:container) {MVCLI::Provisioning::Scope.new provisioner}
+    Given(:scope) {MVCLI::Provisioning::Scope.new provisioner}
     context "with a requirement is specified on the module" do
       Given {mod.requires :foo}
       context "when accessing it but it is not present" do
-        When(:result) {container.evaluate {obj.foo}}
+        When(:result) {scope.evaluate {obj.foo}}
         Then {result.should have_failed MVCLI::Provisioning::UnsatisfiedRequirement, /foo/}
       end
       context "and there is a scope which satisfies the requirement" do
         Given(:foo) {Object.new}
         Given {provisioner[:foo] = foo}
 
-        context "when a dependency is accessed in the context of the container" do
-          When(:result) {container.evaluate {obj.foo}}
+        context "when a dependency is accessed in the context of the scope" do
+          When(:result) {scope.evaluate {obj.foo}}
           Then {result == foo}
         end
       end
