a3bd063b137b0f22491f119edd011f13a95b4317
Bastion <bastionfennell@utexas.edu>
2013-07-08 16:08:18 -0500

    Added tests for validation framework

--- lib/mvcli/middleware/exception_logger/validation_summary.rb
+++ lib/mvcli/middleware/exception_logger/validation_summary.rb
@@ -2,8 +2,20 @@ module MVCLI
   class Middleware
     class ExceptionLogger
       class ValidationSummary
-        def initialize(exception)
-          @exception = exception
+        def initialize(validation)
+          @validation = validation
+        end
+
+        def keys
+          @validation.violations.keys
+        end
+
+        def values
+          @validation.violations.values
+        end
+
+        def write
+          #Write object to a stream
         end
       end
     end
--- spec/mvcli/middleware/exception_logger/validation_summary_spec.rb
+++ spec/mvcli/middleware/exception_logger/validation_summary_spec.rb
@@ -1,6 +1,23 @@
 require "spec_helper"
+require "mvcli/validatable"
+require "mvcli/middleware/exception_logger/validation_summary"
 
 describe "ValidationSummary" do
-  Given(:summary) { ValidationSummary.new error }
+  use_natural_assertions
+  Given(:validator) { MVCLI::Validatable::Validator.new }
+  Given(:object) { Object.new }
+  Given(:validation) { validator.validate object }
+  Given(:summary) { MVCLI::Middleware::ExceptionLogger::ValidationSummary.new validation }
+  context "With a simple validation" do
+    Given do
+      validator.validates(:foo, "You dun goofed", nil: true) {|foo| foo != nil}
+      validator.validate object
+    end
 
+    context "When validation fails" do
+      When { object.stub(:foo) }
+      Then { summary.keys.first == "foo" }
+      And { summary.values.first == ["You dun goofed"] }
+    end
+  end
 end
