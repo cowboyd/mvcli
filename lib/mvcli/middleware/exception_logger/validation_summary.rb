4378efbd88f354c43ff55e5c20efb1f578925e23
Charles Lowell <cowboyd@thefrontside.net>
2013-07-10 22:14:37 -0500

    validation summarizer to interpret validation failures

--- lib/mvcli/middleware/exception_logger/validation_summary.rb
+++ lib/mvcli/middleware/exception_logger/validation_summary.rb
@@ -1,21 +1,40 @@
 module MVCLI
   class Middleware
     class ExceptionLogger
-      class ValidationSummary
+      class ValidationSummary < Enumerator
         def initialize(validation)
+          super &method(:yield)
           @validation = validation
         end
 
-        def keys
-          @validation.violations.keys
+        def yield(output)
+          @validation.violations.each do |key, messages|
+            output << [key, messages]
+          end
+          @validation.errors.each do |key, exceptions|
+            output << [key, exceptions.map {|ex| "#{ex.class}: #{ex.message}"}]
+          end
+          @validation.each do |name, child|
+            if child.length > 1
+              child.each_with_index do |c, i|
+                ValidationSummary.new(c).each do |key, messages|
+                  output << ["#{name}[#{i}].#{key}", messages]
+                end
+              end
+            elsif first = child.first
+              ValidationSummary.new(first).each do |key, messages|
+                output << ["#{name}.#{key}", messages]
+              end
+            end
+          end
         end
 
-        def values
-          @validation.violations.values
-        end
-
-        def write
-          #Write object to a stream
+        def write(stream)
+          each do |key, messages|
+            messages.each do |msg|
+              stream.puts "#{key}: #{msg}"
+            end
+          end
         end
       end
     end
--- spec/mvcli/middleware/exception_logger/validation_summary_spec.rb
+++ spec/mvcli/middleware/exception_logger/validation_summary_spec.rb
@@ -8,16 +8,39 @@ describe "ValidationSummary" do
   Given(:object) { Object.new }
   Given(:validation) { validator.validate object }
   Given(:summary) { MVCLI::Middleware::ExceptionLogger::ValidationSummary.new validation }
-  context "With a simple validation" do
+  Given(:rollup) { summary.each }
+
+  context "with a simple validation" do
     Given do
       validator.validates(:foo, "You dun goofed", nil: true) {|foo| foo != nil}
       validator.validate object
     end
 
-    context "When validation fails" do
+    context "when validation fails" do
       When { object.stub(:foo) }
-      Then { summary.keys.first == "foo" }
-      And { summary.values.first == ["You dun goofed"] }
+      Then { rollup.first == ["foo", ["You dun goofed"]] }
+    end
+    context "when there is an access error" do
+      When { object.stub(:foo) {fail "bad access"}}
+      Then { rollup.first == ["foo", ["RuntimeError: bad access"]] }
+    end
+    context "when the validation fails on an array property" do
+      When {object.stub(:foo) {[nil]}}
+      Then {rollup.first == ["foo[0]", ["You dun goofed"]]}
+    end
+  end
+  context "with a nested validation" do
+    Given(:child) { double(:Child) }
+    Given(:child_validator) { MVCLI::Validatable::Validator.new }
+    Given do
+      object.stub(:bars) {[child, child]}
+      validator.validates_child :bars
+      child_validator.validates(:bazzes, "bad baz") {|baz| not baz.nil? }
+      child.stub(:validation) {child_validator.validate child}
+    end
+    context "with an invalid nested associtaion" do
+      When { child.stub(:bazzes) {['valid', nil, 'valid']} }
+      Then { rollup.first == ["bars[0].bazzes[1]", ["bad baz"]] }
     end
   end
 end
