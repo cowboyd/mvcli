613c25fb6862a3c5704e3a30d810da9e29571f74
Charles Lowell <cowboyd@thefrontside.net>
2013-08-22 16:30:42 -0500

    loader can load arbitrary extensions from a given path

--- lib/mvcli/loader.rb
+++ lib/mvcli/loader.rb
@@ -7,5 +7,30 @@ module MVCLI
     def load(type, name, *args, &block)
       constantize(camelize("#{name}_#{type}")).new(*args, &block)
     end
+
+    def read(path, namespace, extension_type, name)
+      filename = "#{pluralize extension_type}/#{name}_#{extension_type}.rb"
+      bytes = path.read filename
+      eval bytes, blank_slate, filename, 1
+      components = [namespace.name, classify("#{name}_#{extension_type}")]
+      components.shift if namespace == Object
+      lookup components.join('::'), filename
+    end
+
+    def exists?(path, extension_type, name)
+      path.exists? "#{pluralize extension_type}/#{name}_#{extension_type}.rb"
+    end
+
+    private
+
+    def blank_slate
+      BasicObject.new.instance_eval { Kernel.binding }
+    end
+
+    def lookup(class_name, filename)
+      constantize class_name
+    rescue NameError
+      fail LoadError, "expected #{filename} to define #{class_name}"
+    end
   end
 end
--- spec/mvcli/loader_spec.rb
+++ spec/mvcli/loader_spec.rb
@@ -2,22 +2,49 @@ require "spec_helper"
 require "mvcli/loader"
 
 describe "MVCLI::Loader" do
-  Given do
-    class ::TotallyAwesomeController
-      attr_reader :params
-      attr_reader :block
-      def initialize(params, &block)
-        @params = params
-        @block = block
+  use_natural_assertions
+
+  Given(:path) { double :Path }
+  Given(:loader) {MVCLI::Loader.new}
+  Given { path.stub(:read) { content }}
+
+  describe "querying if an extension exists" do
+    context "when it does not exists in the path" do
+      Given { path.stub(:exists?) { false } }
+      Then { not loader.exists? path, :provider, 'bar' }
+      And { path.should have_received(:exists?).with 'providers/bar_provider.rb' }
+    end
+    context "when it does exist in the path" do
+      Given { path.stub(:exists?) { true } }
+      Then { loader.exists? path, :provider, 'bar' }
+
+      context "when the extension is read and it matches the naming conventions" do
+        Given { module ::ANamespace; end }
+        Given(:content) { "class ANamespace::BarProvider; def barf; end; end" }
+        When(:provider_class) { loader.read path, ANamespace, :provider, 'bar' }
+        Then { path.should have_received(:read).with('providers/bar_provider.rb') }
+        Then { not provider_class.nil? }
+        Then { defined? ::ANamespace::BarProvider }
+        And { provider_class == ::ANamespace::BarProvider }
+        And { provider_class.method_defined? :barf }
+        after do
+          Object.send :remove_const, :ANamespace
+        end
+      end
+      context "when extension does not match naming conventions" do
+        Given(:content) { "class ArgleBargle; end" }
+        When(:result) { loader.read path, Object, :provider, 'bar' }
+        Then { result.should have_failed LoadError }
+      end
+      context "when extension matches naming conventions but is not in the right namespace" do
+        Given { module ::OtherNamespace; end }
+        Given(:content) { "class Object::BarProvider; end" }
+        When(:result) { loader.read path, OtherNamespace, :provider, 'bar' }
+        Then { result.should have_failed LoadError }
+        after do
+          Object.send :remove_const, :OtherNamespace
+        end
       end
     end
-  end
-  Given(:loader) {MVCLI::Loader.new}
-
-  context "when a controller load is requested in the global  namespace" do
-    When(:controller){loader.load(:controller, 'totally_awesome', {:foo => :bar}) {'whee!'}}
-    Then {controller.should be_instance_of TotallyAwesomeController}
-    And {controller.params.should eql ({:foo => :bar})}
-    And {controller.block.call.should eql "whee!"}
   end
 end
