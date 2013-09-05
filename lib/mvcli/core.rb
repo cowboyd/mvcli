c4849ac019d5e12490e97c4df2532d727af289cd
Charles Lowell <cowboyd@thefrontside.net>
2013-09-05 08:33:52 -0500

    make core smarter about how namespace and path are derived
    * namespace is inferred via class name
    * default name is inferred from namespace
    * both namespace and path can be explicitly set on either the instance or class level

--- lib/mvcli/core.rb
+++ lib/mvcli/core.rb
@@ -1,24 +1,33 @@
 require "map"
 require "mvcli/path"
+require "mvcli/provisioning"
 
 module MVCLI
   class ExtensionNotFound < StandardError; end
+  class InvalidPath < StandardError; end
 
   class Core
-    attr_reader :loader
-    attr_reader :namespace
-    attr_reader :path
+    requires :loader
+    attr_accessor :path, :namespace
 
     def initialize(options = {})
       options = Map options
-      @path = options[:path] or fail ArgumentError
-      @path = MVCLI::Path.new(@path) if @path.is_a?(String)
-      @loader = options[:loader] or fail ArgumentError
-      @namespace = options[:namespace] || Object
+      @path = options[:path]
+      @namespace = options[:namespace]
+    end
+
+    def path
+      path = @path || self.class.path
+      fail InvalidPath, "core cannot have a nil path" unless path
+      path.is_a?(String) ? MVCLI::Path.new(path) : path
+    end
+
+    def namespace
+      @namespace || self.class.namespace || enclosing_namespace || Object
     end
 
     def exists?(extension_type, name)
-      @loader.exists? @path, extension_type, name
+      loader.exists? @path, extension_type, name
     end
 
     def read(extension_type, name, options = {})
@@ -26,11 +35,26 @@ module MVCLI
         fail ExtensionNotFound, "unable to locate #{extension_type} '#{name}'"
       end
       this = self
-      @loader.read(path, extension_type, name, namespace).tap do |ext|
+      loader.read(path, extension_type, name, namespace).tap do |ext|
         [ext, ext.singleton_class].each do |cls|
           cls.send(:define_method, :core) { this } if cls.respond_to?(:define_method, true)
         end
       end
+    end
+
+    private
+
+    def enclosing_namespace
+      if self.class != MVCLI::Core && name = self.class.name
+        components = name.split('::')[0..-2]
+        unless components.empty?
+          eval components.join('::')
+        end
+      end
+    end
+
+    class << self
+      attr_accessor :path, :namespace
     end
   end
 end
--- spec/mvcli/core_spec.rb
+++ spec/mvcli/core_spec.rb
@@ -2,33 +2,74 @@ require "spec_helper"
 
 describe "MVCLI Cores" do
   use_natural_assertions
-  Given(:core) { MVCLI::Core.new path: path, namespace: namespace, loader: loader }
-  Given(:path) { double :Path }
-  Given(:namespace) { Module.new }
-  Given(:loader) { double :Loader }
-  Given do
-    loader.stub(:exists?) { true }
-  end
 
-  describe "the query interface" do
-    Then { core.exists? :provider, 'naming' }
-    And { loader.should have_received(:exists?).with(path, :provider, 'naming') }
-  end
+  Given(:loader) { double(:Loader) }
+  Given { core.stub(:loader) { loader } }
+  Given { loader.stub(:exists?) { true } }
 
-  describe "reading the extension into memory" do
-    context "when it does not exist" do
-      Given { loader.stub(:exists?) { false } }
-      When(:result) { core.read :provider, 'naming' }
-      Then { result.should have_failed MVCLI::ExtensionNotFound  }
+
+  describe "with explicit values for path and namespace" do
+    Given(:core) { MVCLI::Core.new path: path, namespace: namespace}
+    Given(:path) { double(:Path) }
+    Given(:namespace) { Module.new }
+
+
+    describe "the query interface" do
+      Then { core.exists? :provider, 'naming' }
+      And { loader.should have_received(:exists?).with(path, :provider, 'naming') }
     end
-    context "when it does exist" do
-      Given(:artifact) { Class.new }
-      Given { loader.stub(:read) { artifact } }
-      When(:result) { core.read :provider, 'naming' }
-      Then { loader.should have_received(:read).with(path, :provider, 'naming', namespace) }
-      And { result == artifact }
-      And { result.core == core }
-      And { result.new.core == core }
+
+    describe "reading the extension into memory" do
+      context "when it does not exist" do
+        Given { loader.stub(:exists?) { false } }
+        When(:result) { core.read :provider, 'naming' }
+        Then { result.should have_failed MVCLI::ExtensionNotFound  }
+      end
+      context "when it does exist" do
+        Given(:artifact) { Class.new }
+        Given { loader.stub(:read) { artifact } }
+        When(:result) { core.read :provider, 'naming' }
+        Then { loader.should have_received(:read).with(path, :provider, 'naming', namespace) }
+        And { result == artifact }
+        And { result.core == core }
+        And { result.new.core == core }
+      end
+    end
+  end
+
+  describe "with no explicit values for path and namespace" do
+    Given(:core) { MVCLI::Core.new  }
+    When(:result) { core.path }
+    Then { result.should have_failed MVCLI::InvalidPath }
+    Then { core.namespace == Object }
+  end
+
+  describe "resolving the namespace" do
+    When(:namespace) { core.namespace }
+
+    describe "when the class is anonymous" do
+      Given(:core) { Class.new(MVCLI::Core).new }
+      Then { namespace == Object }
+      describe "but the namespace attribute is set" do
+        Given { core.class.namespace = MVCLI }
+        Then { namespace == MVCLI }
+      end
+    end
+    describe "when the class is named" do
+      Given(:core) do
+        module MVCLI
+          module Test
+            class MyCore < MVCLI::Core
+              new
+            end
+          end
+        end
+      end
+      Then { namespace == MVCLI::Test }
+      describe "but the namespace class attribute is set" do
+        Given { core.class.namespace = MVCLI }
+        Then { namespace == MVCLI }
+      end
     end
   end
 end
